b6039fea1222754375b2ed8cfd96e7b79cc6bcf3
Jose Pereyra <j.pereyra@sien.in>
2020-03-11 17:02:36 -0700

    nose que mas mamadas le puse

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 import * as PIXI from 'pixi.js';
 import fragment from './shader/fragment.glsl';
+import $ from 'jquery';
 
-let img = require('./stars.jpg');
+let img = require('./meow.jpg');
 let img1 = require('./mina.jpg');
 let img2 = require('./weather.jpg');
 // The application will create a renderer using WebGL, if possible,
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container.
 const app = new PIXI.Application(window.innerWidth, window.innerHeight,{
-    autoResize:true,
+    autoResize: true
 });
  
 // The application will create a canvas element for you that you
@@ -29,21 +30,40 @@ loader.load((loader, resources) => {
 
     let filter = new PIXI.Filter(null, fragment);
 
+    // smokeFilter = new PIXI.Filter(null, fragment);
+
+    // smokeFilter
+    filter.apply = function(filterManager, input, output, clear) {
+        const matrix = new PIXI.Matrix();
+        this.uniforms.mappedMatrix = filterManager.calculateNormalizedScreenSpaceMatrix(matrix);
+
+        PIXI.Filter.prototype.apply.call(
+            this,
+            filterManager,
+            input,
+            output,
+            clear
+        );
+    };
     // This creates a texture from a 'bunny.png' image.
     const bunny = new PIXI.Sprite(resources.img0.texture);
  
     // Setup the position of the bunny
-    bunny.x = app.renderer.width / 2;
-    bunny.y = app.renderer.height / 2;
+    // bunny.x = app.renderer.width / 2;
+    // bunny.y = app.renderer.height / 2;
  
-    // Rotate around the center
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
+    // Rotate around the centerd
+    //  bunny.anchor.x = 0.5;
+    // bunny.anchor.y = 0.5;
+
+    let winAspect = $(window).width()/$(window).height();
+    
+    let imageAspect = resources.img1.texture.width/resources.img1.texture.height;
 
     //FILTERS
     bunny.filters = [filter];
 
-    Filter.uniforms.uTextureone = resources.img0.texture;
+    //filter.uniforms.uTextureone = resources.img0.texture;
  
     // Add the bunny to the scene we are building.
     app.stage.addChild(bunny);
@@ -51,7 +71,7 @@ loader.load((loader, resources) => {
     // Listen for frame updates
     app.ticker.add(() => {
          // each frame we spin the bunny around a bit
-        bunny.rotation += 0.01;
+        //bunny.rotation += 0.01;
     });
 });
 
--- shader/fragment.glsl
+++ shader/fragment.glsl
@@ -3,14 +3,18 @@
 uniform sampler2D uSampler;
 uniform sampler2D uTextureOne;
 uniform sampler2D uTextureTwo;
-
 varying vec2 vTextureCoord;
+uniform mat3 mappedMatrix;
 
 
 void main() {
 
-    gl_FragColor = vec4(1.,1.,0.,1.);
+    vec3 map = vec3(vTextureCoord.xy,1.)*mappedMatrix;
+    vec2 uv = (map.xy - 0.5) + 0.5;
 
-    vec2 uv = vec2(vTextureCoord.x + sin(vTextureCoord.y*10.)/10., vTextureCoord.y);
-    gl_FragColor = texture2D(uTextureOne, vTextureCoord);
+    //gl_FragColor = vec4(1.,1.,0.,1.);
+
+   //vec2 uv = vec2(vTextureCoord.x + sin(vTextureCoord.y*10.)/10., vTextureCoord.y);
+    gl_FragColor = texture2D(uTextureOne, uv);
+    //gl_FragColor =vec4(uv,0.,1.);
 }
