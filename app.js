d3dc19b593f1ac7ea326da389b6d0c728b1752f0
Jose Pereyra <j.pereyra@sien.in>
2020-03-12 17:01:17 -0700

    imagenes done\

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import * as PIXI from 'pixi.js';
 import fragment from './shader/fragment.glsl';
 import $ from 'jquery';
+import {TweenMax, Power3} from 'gsap';
+
 
 let img = require('./meow.jpg');
 let img1 = require('./mina.jpg');
@@ -9,7 +11,7 @@ let img2 = require('./weather.jpg');
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container.
 const app = new PIXI.Application(window.innerWidth, window.innerHeight,{
-    autoResize: true
+    resizeTo: $(window).width()
 });
  
 // The application will create a canvas element for you that you
@@ -23,17 +25,16 @@ loader.add('img1', img1);
 loader.add('img2', img2);
 
 
-
 // load the texture we need
 loader.load((loader, resources) => {
     
 
-    let filter = new PIXI.Filter(null, fragment);
+    let Filter = new PIXI.Filter(null, fragment);
 
     // smokeFilter = new PIXI.Filter(null, fragment);
 
     // smokeFilter
-    filter.apply = function(filterManager, input, output, clear) {
+    Filter.apply = function(filterManager, input, output, clear) {
         const matrix = new PIXI.Matrix();
         this.uniforms.mappedMatrix = filterManager.calculateNormalizedScreenSpaceMatrix(matrix);
 
@@ -47,33 +48,59 @@ loader.load((loader, resources) => {
     };
     // This creates a texture from a 'bunny.png' image.
     const bunny = new PIXI.Sprite(resources.img0.texture);
- 
+    
     // Setup the position of the bunny
     // bunny.x = app.renderer.width / 2;
     // bunny.y = app.renderer.height / 2;
  
     // Rotate around the centerd
-    //  bunny.anchor.x = 0.5;
-    // bunny.anchor.y = 0.5;
 
     let winAspect = $(window).width()/$(window).height();
     
     let imageAspect = resources.img1.texture.width/resources.img1.texture.height;
 
+    if(winAspect > imageAspect) {
+        Filter.uniforms.uvAspect = {x:1.,y:imageAspect/winAspect};
+    }
+    else{
+        Filter.uniforms.uvAspect = {x:winAspect/imageAspect, y:1.}
+    }
+
     //FILTERS
-    bunny.filters = [filter];
+    bunny.filters = [Filter];
+
+    //Filter.uniforms.uTextureOne   = resources.img0.texture;
+    Filter.uniforms.uTextureTwo   = resources.img1.texture;
+    Filter.uniforms.uTextureThree = resources.img2.texture;
+    Filter.uniforms.uProgress = 0.;
 
     //filter.uniforms.uTextureone = resources.img0.texture;
  
     // Add the bunny to the scene we are building.
     app.stage.addChild(bunny);
+
+
+    $('a').on('mouseover', function(){
+        let to = $(this).index();
+        TweenMax.to(Filter.uniforms, 1, {
+            uProgress: to,
+            easeOut: Power3.easeOut,
+            onUpdate: ()=>{
+               // console.log(Filter.uniforms.uProgress);
+                let number = Math.floor(Filter.uniforms.uProgress);
+                Filter.uniforms.uTextureOne = resources[`img${number}`].texture;
+                if(number < 2) {
+                    Filter.uniforms.uTextureTwo = resources[`img${number+1}`].texture;
+                }
+            }})
+    });
+
+
  
     // Listen for frame updates
     app.ticker.add(() => {
-         // each frame we spin the bunny around a bit
+        Filter.uniforms.uTime += 0.01;
+        // each frame we spin the bunny around a bit
         //bunny.rotation += 0.01;
     });
 });
-
-
-    
--- shader/fragment.glsl
+++ shader/fragment.glsl
@@ -5,16 +5,36 @@ uniform sampler2D uTextureOne;
 uniform sampler2D uTextureTwo;
 varying vec2 vTextureCoord;
 uniform mat3 mappedMatrix;
+uniform vec2 uvAspect;
+uniform float uProgress;
+
+mat2 rotate(float a) {
+    float s = sin(a);
+    float c = cos(a);
+    return mat2(c, -s, s, c);
+}
 
 
 void main() {
 
     vec3 map = vec3(vTextureCoord.xy,1.)*mappedMatrix;
-    vec2 uv = (map.xy - 0.5) + 0.5;
+    vec2 uv = (map.xy - 0.5)*.5*vec2(0.5,2.) + 0.5 ;
+
+    vec2 uvDivided = fract(uv*vec2(150.,1.));
+    float progress = fract(uProgress);
+
+    vec2 uvDisplaced1 = uv + rotate(3.1415926/4.)*uvDivided*progress*0.1;
+    vec2 uvDisplaced2 = uv + rotate(3.1415926/4.)*uvDivided*(1. - progress)*0.1;
+
+    
+
 
     //gl_FragColor = vec4(1.,1.,0.,1.);
 
    //vec2 uv = vec2(vTextureCoord.x + sin(vTextureCoord.y*10.)/10., vTextureCoord.y);
-    gl_FragColor = texture2D(uTextureOne, uv);
-    //gl_FragColor =vec4(uv,0.,1.);
+   vec4 im1 = texture2D(uTextureOne, uvDisplaced1);
+   vec4 im2 = texture2D(uTextureTwo, uvDisplaced2);
+
+   gl_FragColor = mix(im1, im2, progress);
+    //gl_FragColor =vec4(uvDivided,0.,1.);
 }
